a3821f4697ff109c585f4eda9b0627f73e83711e
hadamelino <hadamuhammad@gmail.com>
2021-03-09 09:10:00 +0700

    Adding initial files

---
+++ Capstone_Week1.py
@@ -0,0 +1,16 @@
+from PIL import Image
+import os
+
+source_path = '/home/<student ID>/images'
+
+dir = os.listdir(source_path)
+
+for file in dir:
+    dir = '/opt/icons'
+    try:
+        if not file.endswith('.jpg'):
+            img = Image.open(dir)
+            img.rotate(270).resize((128, 128)).convert('RGB')
+            img.save(dir + '.jpg')
+    except:
+        continue
