31244e12fde7694ec0d9e9b3a4ae6063b2de6e51
hadamelino <hadamuhammad@gmail.com>
2021-03-09 11:40:03 +0700

    Adding Capstone Week 2 file

---
+++ Capstone_Week2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+
+feedback_path = '/data/feedback'
+
+feedbacks = os.listdir(feedback_path)
+
+result = {}
+
+for feedback in feedbacks:
+   with open (feedback_path + '/' + feedback) as file:
+      data = [line.strip() for line in file.readlines()]
+      result = {"title": data[0], "name": data[1], "date": data[2], "feedback": data[3]}
+   response = requests.post('http://34.67.146.222/feedback/', data=result)
+   response.raise_for_status()
