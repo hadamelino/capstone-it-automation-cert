9bfed7975bb9891f2dcac5e403b967f9425105cd
hadamelino <hadamuhammad@gmail.com>
2021-03-11 01:54:27 +0700

    Rename destination path variable for saving the converted images

--- Capstone_Week1.py
+++ Capstone_Week1.py
@@ -6,11 +6,11 @@ source_path = '/home/<student ID>/images'
 dir = os.listdir(source_path)
 
 for file in dir:
-    dir = '/opt/icons'
+    dest = '/opt/icons'
     try:
         if not file.endswith('.jpg'):
             img = Image.open(dir)
             img.rotate(270).resize((128, 128)).convert('RGB')
-            img.save(dir + '.jpg')
+            img.save(dest + '.jpg')
     except:
         continue
